55c6c0497997070cabe44e0a60d32937a8c38d40
Ioanna Anemopoulou <ioanna@Ioannas-MacBook-Pro.local>
2021-07-28 12:27:15 +0100

    Added assets folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Main from './components/Main';
+import Header from './components/Header';
+import PixiComponent from './components/PixiComponent';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <PixiComponent />
+      <Main />
     </div>
   );
 }
